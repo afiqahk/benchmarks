1b51019002be54dbefee4d9d7d40fab05670f565
afiqahk <47786945+afiqahk@users.noreply.github.com>
2022-08-02 15:16:50 +0800

    Add deque benchmark with different deque size

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Benchmarks
 
-1. [Clearing a deque](https://quick-bench.com/q/EleR1xLtmLRf85kQvQ284uGedk8), [clear_deque.cpp](https://github.com/afiqahk/benchmarks/blob/main/src/clear_deque.cpp)
+1. [Clearing a deque](https://quick-bench.com/q/qL0NKFyhq-ebVVyQgsL_DAk00fc) ([code](https://github.com/afiqahk/benchmarks/blob/main/src/clear_deque.cpp)) - change size of container by setting _Arg(n)_
+    - [with different-sized deques](https://quick-bench.com/q/FkbSkf_wkq6Vc57nZFiWVwo5dYQ) ([code](https://github.com/afiqahk/benchmarks/blob/main/src/clear_deque_n.cpp))
+    - [with different-sized deques, shrink-to-fit version](https://quick-bench.com/q/z_SjnivSX_Tv6WgaNCJr9eyHEGA) ([code](https://github.com/afiqahk/benchmarks/blob/main/src/clear_dequeSTF_n.cpp))
---
+++ src/clear_deque_n.cpp
@@ -0,0 +1,85 @@
+#include <deque>
+
+static void clear_with_assign(benchmark::State& state)
+{
+  std::deque<int> q;
+  for(auto _ : state)
+  {
+    for(size_t i=0; i<state.range(0);++i)
+    {
+      q.push_back(i);
+    }
+    benchmark::DoNotOptimize(q);
+    q = {};
+    benchmark::DoNotOptimize(q);
+  }
+}
+BENCHMARK(clear_with_assign)->Arg(10)->Arg(100)->Arg(250)->Arg(500)->Arg(750);
+
+static void clear_with_pop(benchmark::State& state)
+{
+  std::deque<int> q;
+  for(auto _ : state)
+  {
+    for(size_t i=0; i<state.range(0);++i)
+    {
+      q.push_back(i);
+    }
+    benchmark::DoNotOptimize(q);
+    while(!q.empty())
+    {
+      q.pop_front();
+    }
+    benchmark::DoNotOptimize(q);
+  }
+}
+BENCHMARK(clear_with_pop)->Arg(10)->Arg(100)->Arg(250)->Arg(500)->Arg(750);
+
+static void clear_with_clear(benchmark::State& state)
+{
+  std::deque<int> q;
+  for(auto _ : state)
+  {
+    for(size_t i=0; i<state.range(0);++i)
+    {
+      q.push_back(i);
+    }
+    benchmark::DoNotOptimize(q);
+    q.clear();
+    benchmark::DoNotOptimize(q);
+  }
+}
+BENCHMARK(clear_with_clear)->Arg(10)->Arg(100)->Arg(250)->Arg(500)->Arg(750);
+
+static void clear_with_swap_inplace(benchmark::State& state)
+{
+  std::deque<int> q;
+  for(auto _ : state)
+  {
+    for(size_t i=0; i<state.range(0);++i)
+    {
+      q.push_back(i);
+    }
+    benchmark::DoNotOptimize(q);
+    std::deque<int>().swap(q);
+    benchmark::DoNotOptimize(q);
+  }
+}
+BENCHMARK(clear_with_swap_inplace)->Arg(10)->Arg(100)->Arg(250)->Arg(500)->Arg(750);
+
+static void clear_with_swap(benchmark::State& state)
+{
+  std::deque<int> q;
+  for(auto _ : state)
+  {
+    for(size_t i=0; i<state.range(0);++i)
+    {
+      q.push_back(i);
+    }
+    benchmark::DoNotOptimize(q);
+    std::deque<int> empty;
+    std::swap(q, empty);
+    benchmark::DoNotOptimize(q);
+  }
+}
+BENCHMARK(clear_with_swap)->Arg(10)->Arg(100)->Arg(250)->Arg(500)->Arg(750);
